544b5bb40890d38c67abd688328d4df801f906a5
Serghei Filippov <blastowind@gmail.com>
2015-07-26 00:30:32 -0400

    Fixes libraries

--- lib/cascade.py
+++ lib/cascade.py
@@ -1,1 +1,14 @@
-/Users/Saevon/Projects/Recipe/cascade.py
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from functools import wraps
+
+
+def cascade(func):
+    '''
+    A method wrapper that ensures that self is always returned
+    '''
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        func(self, *args, **kwargs)
+        return self
+    return wrapper
